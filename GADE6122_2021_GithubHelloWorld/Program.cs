795c2cde063b264d3c526ab3cfa061c957738888
pieterjoubert <pieter.joubert@gmail.com>
2021-08-02 09:27:10 +0200

    Changed middle name again.

--- GADE6122_2021_GithubHelloWorld/Program.cs
+++ GADE6122_2021_GithubHelloWorld/Program.cs
@@ -11,7 +11,7 @@ namespace GADE6122_2021_GithubHelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-            Console.WriteLine("Hello Pieter 'Danger' Joubert");
+            Console.WriteLine("Hello Pieter 'Github' Joubert");
         }
     }
 }

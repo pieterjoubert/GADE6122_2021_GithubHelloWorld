ab72e2bbd51faf1e415f7b4a69a43a5cd5208ea9
mattdelta2 <matt.wierenga@outlook.com>
2021-08-02 09:25:21 +0200

    Added Name

--- GADE6122_2021_GithubHelloWorld/Program.cs
+++ GADE6122_2021_GithubHelloWorld/Program.cs
@@ -11,7 +11,12 @@ namespace GADE6122_2021_GithubHelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+
             Console.WriteLine("Hello Pieter Joubert");
+            
+
+            Console.WriteLine("Hello Pieter 'Danger' Joubert");
+
             Console.WriteLine("Matt");
         }
     }
